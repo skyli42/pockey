128b4c6769ce6525da68433b6536cb7941e1f0cf
Sky Li <skylimail42@gmail.com>
2017-03-05 09:37:41 -0500

    D

--- js/game.js
+++ js/game.js
@@ -4,6 +4,6 @@ function setup(){
 	setInterval(draw(), 1000/60);
 
 }
-function draw(){
+function draw(canvas){
 
 }
---
+++ js/rink.js
@@ -0,0 +1,12 @@
+const width = 4700;
+const height = 2000;
+
+module.exports = class Rink{
+	constructor(){
+		this.balls = [];
+
+	}
+	draw(canvas){
+		//tbd
+	}
+}
